2b41ad6b5840e4f87479a7b5eed9eb774f6fdf96
waldanzubary <waldanz329@gmail.com>
2026-02-18 22:16:19 +0700

    auth redirect fix

--- lib/auth.ts
+++ lib/auth.ts
@@ -31,6 +31,9 @@ export async function signUpWithEmail(
 export async function signInWithGoogle() {
     const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
+        options: {
+            redirectTo: window.location.origin,
+        },
     });
     if (error) throw error;
     return data;
